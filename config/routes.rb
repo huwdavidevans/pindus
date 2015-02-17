19f0a05c80f5de8165c52cac8b61cf73dd6cfab8
Huw Evans <huw.evans@wjec.co.uk>
2015-02-17 16:13:34 +0000

    disable registrations on prod

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout'}
-  
+  if Rails.env.production?
+    devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout'}, :controllers => { :registrations => "registrations" } 
+  else
+    devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout'}
+  end
+
   # match ':controller(/:action(/:id))', :via => :get
 
   patch 'pics/updateTags/:id', to: 'pics#updateTags', as: :updateTags
